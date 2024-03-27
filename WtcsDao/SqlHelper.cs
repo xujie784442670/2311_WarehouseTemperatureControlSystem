4a91e588fbe3066ed49eb9f497116c8f6417155d
jason <784442670@qq.com>
2024-03-27 14:57:35 +0800

    1.添加Sqlite帮助类 2.添加Sqlite依赖

---
+++ WtcsDao/SqlHelper.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace AcmsDao;
+
+public class SqlBuilder
+{
+    public string TableName { get; private set; }
+    public List<SqlParam> SqlSets { get; private set; } = new();
+    public List<SqlParam> SqlWheres { get; private set; } = new();
+    public List<SqlParam> SqlInserts { get; private set; } = new();
+
+    public SqlBuilder(string tableName)
+    {
+        TableName = tableName;
+    }
+
+    /// <summary>
+    /// 添加sql参数
+    /// </summary>
+    /// <param name="sqlParams">SQL参数存储集合</param>
+    /// <param name="condition">条件</param>
+    /// <param name="type">SQL类型</param>
+    /// <param name="columnName">列名</param>
+    /// <param name="sql">条件sql</param>
+    /// <param name="parameters">参数列表</param>
+    /// <returns></returns>
+    public SqlBuilder AddSqlParam(List<SqlParam> sqlParams, bool condition, SqlType? type, string columnName,
+        string sql, params SQLiteParameter[] parameters)
+    {
+        if (condition)
+        {
+            sqlParams.Add(new SqlParam
+            {
+                ColumnName = columnName,
+                Sql = sql,
+                Parameters = new List<SQLiteParameter>(parameters),
+                SqlType = type
+            });
+        }
+
+        return this;
+    }
+
+    /// <summary>
+    /// 添加set参数
+    /// </summary>
+    /// <param name="condition">条件</param>
+    /// <param name="columnName">列名</param>
+    /// <param name="value">值</param>
+    /// <returns></returns>
+    public SqlBuilder Set(bool condition, string columnName, object value)
+    {
+        return AddSqlParam(SqlSets, condition, null, columnName, $"{columnName} = @{columnName}",
+            new SQLiteParameter($"@{columnName}", value));
+    }
+
+    /// <summary>
+    /// 添加where参数
+    /// </summary>
+    /// <param name="condition">条件</param>
+    /// <param name="type">条件类型</param>
+    /// <param name="sql">sql</param>
+    /// <param name="parameters">参数</param>
+    /// <returns></returns>
+    public SqlBuilder Where(bool condition, SqlType type, string sql, params SQLiteParameter[] parameters)
+    {
+        return AddSqlParam(SqlWheres, condition, type, "", sql, parameters);
+    }
+
+    /// <summary>
+    /// 添加where参数,默认为And
+    /// </summary>
+    /// <param name="condition">条件</param>
+    /// <param name="sql">sql</param>
+    /// <param name="parameters">参数</param>
+    /// <returns></returns>
+    public SqlBuilder And(bool condition, string sql, params SQLiteParameter[] parameters)
+    {
+        return Where(condition, SqlType.And, sql, parameters);
+    }
+
+    /// <summary>
+    /// 添加where参数,默认为Or
+    /// </summary>
+    /// <param name="condition">条件</param>
+    /// <param name="sql">sql</param>
+    /// <param name="parameters">参数</param>
+    /// <returns></returns>
+    public SqlBuilder Or(bool condition, string sql, params SQLiteParameter[] parameters)
+    {
+        return Where(condition, SqlType.Or, sql, parameters);
+    }
+
+    /// <summary>
+    /// 添加insert参数
+    /// </summary>
+    /// <param name="condition">条件</param>
+    /// <param name="columnName">列名</param>
+    /// <param name="value">值</param>
+    /// <returns></returns>
+    public SqlBuilder Insert(bool condition, string columnName, object value)
+    {
+        return AddSqlParam(SqlInserts, condition, null, columnName, $"@{columnName}",
+            new SQLiteParameter($"@{columnName}", value));
+    }
+
+    /// <summary>
+    /// 构建查询
+    /// </summary>
+    /// <param name="sql">sql输出</param>
+    /// <param name="parameters">参数输出</param>
+    public void BuildQuery(out string sql, out List<SQLiteParameter> parameters)
+    {
+        var sqlTemplate = $"select * from {TableName} where 1=1";
+        parameters = new();
+        foreach (var sqlWhere in SqlWheres)
+        {
+            var type = sqlWhere.SqlType ?? SqlType.And;
+            sqlTemplate += $" {type} {sqlWhere.Sql}";
+            parameters.AddRange(sqlWhere.Parameters);
+        }
+
+        sql = sqlTemplate;
+    }
+
+    /// <summary>
+    /// 构建更新
+    /// </summary>
+    /// <param name="sql">sql输出</param>
+    /// <param name="parameters">参数输出</param>
+    public void BuildUpdate(out string sql, out List<SQLiteParameter> parameters)
+    {
+        var sqlTemplate = $"update {TableName} set ";
+        parameters = new();
+        foreach (var sqlSet in SqlSets)
+        {
+            sqlTemplate += $"{sqlSet.Sql},";
+            parameters.AddRange(sqlSet.Parameters);
+        }
+
+        sqlTemplate = sqlTemplate.TrimEnd(',');
+        sqlTemplate += " where 1=1";
+        foreach (var sqlWhere in SqlWheres)
+        {
+            var type = sqlWhere.SqlType ?? SqlType.And;
+            sqlTemplate += $" {type} {sqlWhere.Sql}";
+            parameters.AddRange(sqlWhere.Parameters);
+        }
+
+        sql = sqlTemplate;
+    }
+
+    /// <summary>
+    /// 构建插入
+    /// </summary>
+    /// <param name="sql">sql输出</param>
+    /// <param name="parameters">参数输出</param>
+    public void BuildInsert(out string sql, out List<SQLiteParameter> parameters)
+    {
+        var sqlTemplate = $"insert into {TableName}";
+        var columns = string.Join(",", SqlInserts.Select(x => x.ColumnName));
+        var values = string.Join(",", SqlInserts.Select(x => x.Sql));
+        sqlTemplate += $"({columns}) values({values})";
+        parameters = new();
+        foreach (var sqlInsert in SqlInserts)
+        {
+            parameters.AddRange(sqlInsert.Parameters);
+        }
+
+        sql = sqlTemplate;
+    }
+
+    /// <summary>
+    /// 构建删除
+    /// </summary>
+    /// <param name="sql">sql输出</param>
+    /// <param name="parameters">参数输出</param>
+    public void BuildDelete(out string sql, out List<SQLiteParameter> parameters)
+    {
+        var sqlTemplate = $"delete from {TableName} where 1=1";
+        parameters = new();
+        foreach (var sqlWhere in SqlWheres)
+        {
+            var type = sqlWhere.SqlType ?? SqlType.And;
+            sqlTemplate += $" {type} {sqlWhere.Sql}";
+            parameters.AddRange(sqlWhere.Parameters);
+        }
+
+        sql = sqlTemplate;
+    }
+}
+
+public class SqlParam
+{
+    public string ColumnName { get; set; }
+    public string Sql { get; set; }
+    public List<SQLiteParameter> Parameters { get; set; }
+    public SqlType? SqlType { get; set; }
+}
+
+public enum SqlType
+{
+    And,
+    Or
+}
---
+++ WtcsDao/SqliteHelper.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AcmsDao
+{
+    public class SqliteHelper
+    {
+        public static bool IsDebug { get; set; } = false;
+        public static bool IsPrintSql { get; set; } = false;
+        public static bool IsPrintResult { get; set; } = false;
+        public static string ConnectionString { get; private set; }
+
+        public static bool IsInit => !string.IsNullOrWhiteSpace(ConnectionString);
+        public static void Println(string guid,string sql,params SQLiteParameter[] parameters)
+        {
+            if(!IsDebug) return;
+            if(!IsPrintSql) return;
+            Debug.WriteLine($"[{guid}] ==> Sql       : {sql}");
+            Debug.WriteLine($"[{guid}] ==> Parameters: {string.Join(",", parameters.Select(p => p.Value))}");
+        }
+
+        public static void Println(string guid, int rows)
+        {
+            if (!IsDebug) return;
+            if (!IsPrintResult) return;
+            Debug.WriteLine($"[{guid}] <== Rows: {rows}");
+        }
+
+        public static void Println(string guid, DataTable dt)
+        {
+            if (!IsDebug) return;
+            if (!IsPrintResult) return;
+            // 打印列名
+            Debug.WriteLine($"[{guid}] <== Columns: {string.Join("\t", dt.Columns.Cast<DataColumn>().Select(c => c.ColumnName))}");
+            // 打印数据
+            foreach (DataRow row in dt.Rows)
+            {
+                Debug.WriteLine($"[{guid}] <== Row: {string.Join("\t", row.ItemArray)}");
+            }
+            Debug.WriteLine($"[{guid}] <== Rows: {dt.Rows.Count}");
+        }
+
+        /// <summary>
+        /// 初始化帮助类
+        /// </summary>
+        /// <param name="dbPath">数据库文件路径</param>
+        /// <param name="pwd">数据库密码(可选)</param>
+        public static void Init(string dbPath, string? pwd = "")
+        {
+            var builder = new SQLiteConnectionStringBuilder
+            {
+                DataSource = dbPath
+            };
+            if (!string.IsNullOrEmpty(pwd))
+            {
+                builder.Password = pwd;
+            }
+
+            ConnectionString = builder.ToString();
+        }
+
+        private static void CheckInit()
+        {
+            if (!IsInit)
+            {
+                throw new System.Exception("未初始化数据库连接,请调用SQLiteHelper.Init方法进行初始化");
+            }
+        }
+
+        private static SQLiteConnection GetConnection()
+        {
+            CheckInit();
+            var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+            return connection;
+        }
+
+        private static SQLiteCommand CreateCommand(string sql, params SQLiteParameter[] parameters)
+        {
+            var cmd = GetConnection().CreateCommand();
+            cmd.CommandText = sql;
+            if (parameters.Length > 0)
+            {
+                cmd.Parameters.AddRange(parameters);
+            }
+            return cmd;
+        }
+
+        // sql的执行
+        // 结果的处理
+
+        public static T? QueryOne<T>(string sql, Func<DataRow, string[], T> resultHandler,
+            params SQLiteParameter[] parameters)
+        {
+            var result = Query(sql, resultHandler, parameters);
+            if(result.Count>1) throw new Exception("查询结果不唯一");
+            return result.Count == 0 ? default : result[0];
+        }
+
+        /// <summary>
+        /// 执行sql语句
+        /// </summary>
+        /// <typeparam name="T">返回结果的实体类型</typeparam>
+        /// <param name="sql">sql语句</param>
+        /// <param name="handler">结果处理器接口实现</param>
+        /// <param name="parameters">参数列表</param>
+        /// <returns></returns>
+        public static List<T> Query<T>(string sql,
+            IResultHandler<T> handler,
+            params SQLiteParameter[] parameters)
+        {
+            return Query(sql, handler.HandleResult, parameters);
+        }
+
+        /// <summary>
+        /// 执行sql语句
+        /// </summary>
+        /// <typeparam name="T">返回结果的实体类型</typeparam>
+        /// <param name="sql">sql语句</param>
+        /// <param name="resultHandler">结果处理器</param>
+        /// <param name="parameters">参数列表</param>
+        /// <returns></returns>
+        public static List<T> Query<T>(string sql, Func<DataRow, string[], T> resultHandler,
+            params SQLiteParameter[] parameters)
+        {
+            using var cmd = CreateCommand(sql, parameters);
+            var guid = Guid.NewGuid().ToString();
+            // 打印sql
+            Println(guid,cmd.CommandText, parameters);
+            // 执行sql语句
+            using var reader = cmd.ExecuteReader();
+            // 将结果转换为DataTable
+            var dt = new DataTable();
+            dt.Load(reader);
+            // 打印结果
+            Println(guid, dt);
+            // 获取列名
+            var columnNames = new string[dt.Columns.Count];
+            for (var i = 0; i < dt.Columns.Count; i++)
+            {
+                columnNames[i] = dt.Columns[i].ColumnName;
+            }
+            // 结果处理
+            var result = new List<T>();
+            foreach (DataRow row in dt.Rows)
+            {
+                result.Add(resultHandler(row, columnNames));
+            }
+            return result;
+        }
+
+        public static T QueryScalar<T>(string sql, params SQLiteParameter[] parameters)
+        {
+            using var cmd = CreateCommand(sql, parameters);
+            var guid = Guid.NewGuid().ToString();
+            // 打印sql
+            Println(guid, cmd.CommandText, parameters);
+            // 执行sql语句
+            var result = cmd.ExecuteReader();
+            var dt = new DataTable();
+            dt.Load(result);
+            // 打印结果
+            Println(guid, dt);
+            var value = dt.Rows[0][0];
+            return dt.Rows.Count > 0 ? (T)value : default;
+        }
+
+        public static int ExecuteNonQuery(string sql, params SQLiteParameter[] parameters)
+        {
+            using var cmd = CreateCommand(sql, parameters);
+            var guid = Guid.NewGuid().ToString();
+            // 打印sql
+            Println(guid, cmd.CommandText, parameters);
+            var rows = cmd.ExecuteNonQuery();
+            Println(guid,rows);
+            return rows;
+        }
+
+
+
+
+        public static long GetLastInsertRowId(string tableName)
+        {
+            return QueryScalar<long>($"select seq from sqlite_sequence where name='{tableName}'");
+        }
+    }
+}
