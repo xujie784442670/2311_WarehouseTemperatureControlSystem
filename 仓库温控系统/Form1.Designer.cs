7f0937709ac45ae41641d46fba55db07fa7b47e5
jj <2431312567@QQ.com>
2024-03-29 09:31:12 +0800

    工具文件

--- 仓库温控系统/Form1.Designer.cs
+++ 仓库温控系统/Form1.Designer.cs
@@ -37,7 +37,6 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
             this.Text = "Form1";
-           
             this.ResumeLayout(false);
 
         }
